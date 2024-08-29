e1303e22cd9a5e27b77cd0df548cc2c691421f0a
Mehr-code <papa.molla8@gmail.com>
2024-08-30 01:10:39 +0330

    commit

--- script.js
+++ script.js
@@ -16,10 +16,12 @@ const completeEl = document.getElementById("complete");
 const completeElInfo = document.getElementById("complete-info");
 const completeBtn = document.getElementById("complete-button");
 
+// Global variable
 let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
 let countdownActive;
+let savedCountdown;
 
 const second = 1000;
 const minute = second * 60;
@@ -71,6 +73,13 @@ function updateCountdown(e) {
   e.preventDefault();
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
+  savedCountdown = {
+    title: countdownTitle,
+    date: countdownDate,
+  };
+
+  localStorage.setItem("countdown", JSON.stringify(savedCountdown));
+
   // Check for valid date
   if (countdownDate === "") alert("Please select a date for countdown");
   else {
@@ -93,9 +102,25 @@ function reset() {
   // Reset values
   countdownTitle = "";
   countdownDate = "";
+  localStorage.removeItem("countdown");
+}
+
+function restorePreviousCountdown() {
+  // Get countdown from localStorage if available
+  if (localStorage.getItem("countdown")) {
+    inputContainer.hidden = true;
+    savedCountdown = JSON.parse(localStorage.getItem("countdown"));
+    countdownTitle = savedCountdown.title;
+    countdownDate = savedCountdown.date;
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOM();
+  }
 }
 
 // Event Listeners
 CountdownForm.addEventListener("submit", updateCountdown);
 countdownElBtn.addEventListener("click", reset);
 completeBtn.addEventListener("click", reset);
+
+// On load, check localStorage
+restorePreviousCountdown();
